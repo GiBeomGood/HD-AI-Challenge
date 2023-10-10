395ee31450e6f628f1872a12342e0b9466bcb590
GiBeomGood <kwonrjatk@unist.ac.kr>
2023-10-10 14:27:46 +0900

    commit

--- submitter.py
+++ submitter.py
@@ -1,11 +1,13 @@
+import numpy as np
 import pandas as pd
 from tqdm import tqdm
-import torch
-from torch.utils.data import TensorDataset, DataLoader
-device = torch.device('cuda')
 
-@torch.no_grad()
-def deep_submitter(model, save_path='data/submission.csv'):
+
+def deep_submitter(model, save_path='data/dl_submission.csv'):
+    import torch
+    from torch.utils.data import TensorDataset, DataLoader
+    device = torch.device('cuda')
+
     y_test = pd.read_csv('data/sample_submission.csv')
     x_test = pd.read_csv('data/test_4dl.csv')
     y_test.loc[x_test.DIST==0, 'CI_HOUR'] = 0
@@ -18,16 +20,33 @@ def deep_submitter(model, save_path='data/submission.csv'):
     model.load_state_dict(torch.load('checkpoints/best_model.pt'))
 
     model.eval()
-    for indices, x in tqdm(test_loader):
-        indices = indices.numpy()
-        x = x.to(device)
-        output = model(x)
-        output = output.exp() - 1
-        output = output.cpu().numpy()
-        y_test.loc[indices, 'CI_HOUR'] = output
+    with torch.no_grad():
+        for indices, x in tqdm(test_loader):
+            indices = indices.numpy()
+            x = x.to(device)
+            output = model(x)
+            output = output.exp() - 1
+            output = output.cpu().numpy()
+            y_test.loc[indices, 'CI_HOUR'] = output
     
     y_test.loc[y_test.CI_HOUR<0, 'CI_HOUR'] = 0
     
+    if save_path is not None:
+        y_test.to_csv(save_path, encoding='UTF-8', index=False)
+    
+    return y_test
+
+
+def xgb_submitter(model, save_path='data/xgb_submission.csv'):
+    y_test = pd.read_csv('data/sample_submission.csv')
+    x_test = pd.read_csv('data/test_4xgb.csv')
+    y_test.loc[x_test.DIST==0, 'CI_HOUR'] = 0
+
+    y_pred = model.predict(x_test.loc[x_test.DIST!=0, :].values)
+    y_pred = np.exp(y_pred) - 1
+    y_test.loc[x_test.DIST!=0, 'CI_HOUR'] = y_pred
+    y_test.loc[y_test.CI_HOUR<0, 'CI_HOUR'] = 0
+
     if save_path is not None:
         y_test.to_csv(save_path, encoding='UTF-8', index=False)
     
